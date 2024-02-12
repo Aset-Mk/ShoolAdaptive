be6703d18bdf237ca481644f89e09537319ebdf4
Aset-Mk <159731514+Aset-Mk@users.noreply.github.com>
2024-02-12 14:04:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let number=7;
 let namethems=["1) Тема: Производная", "1) Тема: Вероятность"]
 let thems=["proizvod", "veroat"]
 let index=0;
+let repeat=false
+let repeatindex=0;
 function view(){
     const answer = document.querySelector(".answer")
     answer.style.display="block"
@@ -17,7 +19,16 @@ function next(){
     
     let zx=Math.floor(Math.random() * 10) + 1;
     
-    if(zx>5){
+    if (repeat){
+        repeatindex++
+        if(repeatindex==10){
+            repeatindex=0;
+            repeat=false
+        }
+        const head = document.querySelector(".head p")
+        head.innerText = namethems[0];
+
+
         let rand=Math.floor(Math.random() * 23) + 1;
         index=0
         if (rand%2==0){
@@ -38,31 +49,68 @@ function next(){
     
         const buttonanswer = document.querySelector(".button-answer")
         buttonanswer.style.display="none"
-        
-        const head = document.querySelector(".head")
-        head.style.display="none"
-
     }else{
-        let rand=Math.floor(Math.random() * 2) + 1;
-        index=1
-        if (rand%2==0){
-
-        }else{
-            rand--
-        }
-        if(rand==0){
-            rand=2;
-        }
-        number=rand-1;
-        console.log(rand)
-        const question = document.querySelector(".question")
-        question.src="problems/"+thems[index]+"/1 ("+rand+").jpg"
-        
-        const answer = document.querySelector(".answer")
-        answer.style.display="none"
+        if(zx>5){
+            const head = document.querySelector(".head p")
+            head.innerText = namethems[0];
     
-        const buttonanswer = document.querySelector(".button-answer")
-        buttonanswer.style.display="none" 
+    
+            let rand=Math.floor(Math.random() * 23) + 1;
+            index=0
+            if (rand%2==0){
+    
+            }else{
+                rand--
+            }
+            if(rand==0){
+                rand=2;
+            }
+            number=rand-1;
+            console.log(rand)
+            const question = document.querySelector(".question")
+            question.src="problems/"+thems[index]+"/1 ("+rand+").jpg"
+        
+            const answer = document.querySelector(".answer")
+            answer.style.display="none"
+        
+            const buttonanswer = document.querySelector(".button-answer")
+            buttonanswer.style.display="none"
+            
+            
+    
+        }else{
+            const head = document.querySelector(".head p")
+            head.innerText = namethems[1];
+    
+    
+            let rand=Math.floor(Math.random() * 2) + 1;
+            index=1
+            if (rand%2==0){
+    
+            }else{
+                rand--
+            }
+            if(rand==0){
+                rand=2;
+            }
+            number=rand-1;
+            console.log(rand)
+            const question = document.querySelector(".question")
+            question.src="problems/"+thems[index]+"/1 ("+rand+").jpg"
+            
+            const answer = document.querySelector(".answer")
+            answer.style.display="none"
+        
+            const buttonanswer = document.querySelector(".button-answer")
+            buttonanswer.style.display="none" 
+        }
+
     }
     
+    
+}
+
+function nextnone(){
+    repeat=true
+    next()
 }
